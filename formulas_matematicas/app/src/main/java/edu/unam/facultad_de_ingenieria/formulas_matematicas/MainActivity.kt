08e22bb29d097f699c31b34626da44f9870b61dd
Omar Alejandro Sánchez Pérez <osanchezperez70@gmail.com>
2023-10-26 16:16:39 -0600

    First commit

---
+++ formulas_matematicas/app/src/main/java/edu/unam/facultad_de_ingenieria/formulas_matematicas/MainActivity.kt
@@ -0,0 +1,19 @@
+/*
+* Universidad Nacional Autónoma de México
+* Facultad de Ingeniería
+* Semestre 2024 - 1
+* Alumno: Sánchez Pérez Omar Alejandro
+* Asignatura: Cómputo móvil
+* */
+
+package edu.unam.facultad_de_ingenieria.formulas_matematicas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
