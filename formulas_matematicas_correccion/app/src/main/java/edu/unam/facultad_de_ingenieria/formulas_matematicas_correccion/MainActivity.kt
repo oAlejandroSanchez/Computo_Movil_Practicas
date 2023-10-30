4354a8f945ea722fdffd9ce49a6341b032f418db
Omar Alejandro Sánchez Pérez <osanchezperez70@gmail.com>
2023-10-30 09:47:43 -0600

    Corrección de la práctica

--- formulas_matematicas_correccion/app/src/main/java/edu/unam/facultad_de_ingenieria/formulas_matematicas_correccion/MainActivity.kt
+++ formulas_matematicas_correccion/app/src/main/java/edu/unam/facultad_de_ingenieria/formulas_matematicas_correccion/MainActivity.kt
@@ -1,52 +1,45 @@
-/*
-* Universidad Nacional Autónoma de México
-* Facultad de Ingeniería
-* Semestre 2024 - 1
-* Alumno: Sánchez Pérez Omar Alejandro
-* Asignatura: Cómputo móvil
-* */
+package edu.unam.facultad_de_ingenieria.formulas_matematicas_correccion
 
-package edu.unam.facultad_de_ingenieria.formulas_matematicas
-
-import android.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AlertDialog
-import edu.unam.facultad_de_ingenieria.formulas_matematicas.databinding.FormulasMatematicasBinding
+import edu.unam.facultad_de_ingenieria.formulas_matematicas_correccion.databinding.FormulasMatematicasBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: FormulasMatematicasBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding =  FormulasMatematicasBinding.inflate(layoutInflater)
+        //setContentView(R.layout.formulas_matematicas)
+        binding =FormulasMatematicasBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        /* Texto del botón */
-        binding.btnCalcular.text = "Calcular"
+        // Se le da el texto del botón
+        binding.btnCalcular.text = getString(R.string.btnName)
 
-        /* Se configura el CardView con el texto y opciones del Spinner */
-        binding.selectOptionsText.text = "Selecciona una fórmula"
-        val options = arrayOf("Ecuación de segundo grado", "Pitágoras", "Gravitación Universal")
-        val adapter = ArrayAdapter(this, R.layout.simple_spinner_item, options)
-        adapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
+        // Se define el texto del TextView del inicio
+        binding.selectOptionsText.text = getString(R.string.seleccionaFormula)
+
+        // Se le dan las opciones al Spinner
+        val adapter = ArrayAdapter.createFromResource(this, R.array.opcionesSpinner, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         binding.spinnerOptions.adapter = adapter
 
-        // Variable de la respuesta final
-        binding.respuesta.text = "Respuesta: "
-        var respuesta:String = ""
+        // Se coloca el título del segundo TextView
+        binding.respuestaTitle.text = getString(R.string.respuestaTitulo)
+
+        // Se le da instrucciones al usuario
+        binding.msgUsuario.text = getString(R.string.msgUsuario)
 
         // Se declaran las variables donde se guardarán los datos
+        var respuesta:String = ""
         var dato1:String = ""
         var dato2:String = ""
         var dato3:String = ""
 
-        // Instrucciones al usuario
-        binding.msgUsuario.text = "Ingresa los valores de la ecuación: "
-
-        // Añade el evento de selección
+        // Se añade el evento del Spinner
         binding.spinnerOptions.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 // Se realiza una acción en función de la opción seleccionada
@@ -54,7 +47,14 @@ class MainActivity : AppCompatActivity() {
                     // Ecuación de segundo grado
 
                     // Se limpia el text view
-                    binding.respuesta.text = "Respuesta: "
+                    binding.respuesta.text = ""
+                    dato1 = ""
+                    dato2 = ""
+                    dato3 = ""
+
+
+                    // Se cambia la imagen de la ecuación
+                    binding.imagen.setImageResource(R.drawable.chicharronera)
 
                     // Se le da un nombre a los inputs
                     binding.variable1.hint = "a"
@@ -75,22 +75,28 @@ class MainActivity : AppCompatActivity() {
                             respuesta = chicharronera(dato1.toDouble(),dato2.toDouble(),dato3.toDouble())
 
                             if (respuesta == "") {
-                                mensajeUsuario("Error",
-                                    "El valor de 'a' no debe ser cero.")
+                                mensajeUsuario(getString(R.string.errorTitle),
+                                    getString(R.string.msgIndefinido1))
                             }
                         } else {
-                            mensajeUsuario("Advertencia",
-                                "Debe llenar todos los campos.")
+                            mensajeUsuario(getString(R.string.advertenciaTitle),
+                                getString(R.string.msgLlenarInputs))
                         }
 
-                        binding.respuesta.text = "Respuesta: $respuesta"
+                        binding.respuesta.text = "$respuesta"
                     }
 
                 } else if(position == 1) {
                     // Pitágoras
 
                     // Se limpia el text view
-                    binding.respuesta.text = "Respuesta: "
+                    binding.respuesta.text = ""
+                    dato1 = ""
+                    dato2 = ""
+                    dato3 = ""
+
+                    // Se cambia la imagen de la ecuación
+                    binding.imagen.setImageResource(R.drawable.pitagoras)
 
                     // Se le da un nombre a los inputs
                     binding.variable1.hint = "a"
@@ -108,18 +114,24 @@ class MainActivity : AppCompatActivity() {
                         if(!camposVacios(dato1,dato2,"No es necesario este valor")) {
                             respuesta = pitagoras(dato1.toDouble(),dato2.toDouble())
                         } else {
-                            mensajeUsuario("Advertencia",
-                                "Debe llenar todos los campos.")
+                            mensajeUsuario(getString(R.string.advertenciaTitle),
+                                getString(R.string.msgLlenarInputs))
                         }
 
-                        binding.respuesta.text = "Respuesta: $respuesta"
+                        binding.respuesta.text = "$respuesta"
                     }
 
                 } else if(position == 2) {
                     // Gravitación universal
 
                     // Se limpia el text view
-                    binding.respuesta.text = "Respuesta: "
+                    binding.respuesta.text = ""
+                    dato1 = ""
+                    dato2 = ""
+                    dato3 = ""
+
+                    // Se cambia la imagen de la ecuación
+                    binding.imagen.setImageResource(R.drawable.gravitacion_universal)
 
                     // Se le da un nombre a los inputs
                     binding.variable1.hint = "m1"
@@ -139,20 +151,20 @@ class MainActivity : AppCompatActivity() {
                             respuesta = gravitacionUniversal(dato1.toDouble(),dato2.toDouble(),dato3.toDouble())
 
                             if (respuesta == "1") {
-                                mensajeUsuario("Error",
-                                    "El valor de 'r' no debe ser cero.")
+                                mensajeUsuario(getString(R.string.errorTitle),
+                                    getString(R.string.msgIndefinido2))
                                 respuesta = ""
                             } else if(respuesta == "2") {
-                                mensajeUsuario("Error",
-                                    "El valor de las masas debe ser mayor que cero.")
+                                mensajeUsuario(getString(R.string.errorTitle),
+                                    getString(R.string.msgMasas))
                                 respuesta = ""
                             }
                         } else {
-                            mensajeUsuario("Advertencia",
-                                "Debe llenar todos los campos.")
+                            mensajeUsuario(getString(R.string.advertenciaTitle),
+                                getString(R.string.msgLlenarInputs))
                         }
 
-                        binding.respuesta.text = "Respuesta: $respuesta"
+                        binding.respuesta.text = "$respuesta"
                     }
                 }
             }
@@ -221,6 +233,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun mensajeUsuario(title:String, msg:String) {
-        AlertDialog.Builder(this@MainActivity).setTitle(title).setMessage(msg).setPositiveButton("Aceptar") { dialog, _ -> dialog.dismiss() }.show()
+        AlertDialog.Builder(this@MainActivity).setTitle(title).setMessage(msg).setPositiveButton(getString(R.string.btnAceptar)) {
+                dialog, _ -> dialog.dismiss()
+        }.show()
     }
 }
---
+++ formulas_matematicas_correccion/app/src/test/java/edu/unam/facultad_de_ingenieria/formulas_matematicas_correccion/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package edu.unam.facultad_de_ingenieria.formulas_matematicas_correccion
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
